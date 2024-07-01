19b805eee293e8ccb3369c85bad744658454318b
rei <67372053+reiugit@users.noreply.github.com>
2024-07-01 13:54:25 +0200

    Projektdateien hinzufügen.

---
+++ DateTimeOffsetVsDateTime/Printer.cs
@@ -0,0 +1,7 @@
+﻿namespace DateTimeOffsetVsDateTime;
+
+internal static class Printer
+{
+    internal static void Print(string info, DateTime d) => Console.WriteLine($"  {info + ":",-25}  {d}    (Kind = {d.Kind})");
+    internal static void Print(string info, DateTimeOffset d) => Console.WriteLine($"  {info + ":",-25}  {d}");
+}
---
+++ DateTimeOffsetVsDateTime/Program.cs
@@ -0,0 +1,37 @@
+﻿using static DateTimeOffsetVsDateTime.Printer;
+
+// DateTimeOffset
+Console.WriteLine("\nDateTimeOffset (with offset):");
+Print("UtcNow", DateTimeOffset.UtcNow);
+Print("Now", DateTimeOffset.Now);
+
+// DateTime
+Console.WriteLine("\nDateTime (without offset):");
+Print("UtcNow", DateTime.UtcNow);
+Print("Now", DateTime.Now);
+
+// Changing TimeZone
+var (utcNow, now) = (DateTimeOffset.UtcNow, DateTimeOffset.Now);
+Console.WriteLine("\nChanging TimeZone:");
+Print("UtcNow as LocalTime", utcNow.ToLocalTime());
+Print("Now    as Utc", now.ToUniversalTime());
+Print("Now    TimeZone changed", now.ToOffset(TimeSpan.FromHours(5)));
+
+//Converting to DateTime
+Console.WriteLine("\nConverting to DateTime:");
+var utcNow_AsDateTime = utcNow.DateTime;           // Kind=Unspecified
+var utcNow_AsLocalDateTime = utcNow.LocalDateTime; // Kind=Local
+var utcNow_AsUtcDateTime = utcNow.UtcDateTime;     // Kind=Utc
+var now_AsDateTime = now.DateTime;                 // Kind=Unspecified
+var now_AsLocalDateTime = now.LocalDateTime;       // Kind=Local
+var now_AsUtcDateTime = now.UtcDateTime;           // Kind=Utc
+Print("UtcNow  as DateTime", utcNow_AsDateTime);
+Print("UtcNow  as LocalDateTime", utcNow_AsLocalDateTime);
+Print("UtcNow  as UtcDateTime", utcNow_AsUtcDateTime);
+Print("Now     as DateTime", now_AsDateTime);
+Print("Now     as LocalDateTime", now_AsLocalDateTime);
+Print("Now     as UtcDateTime", now_AsUtcDateTime);
+
+
+Console.WriteLine("\n\nPress any key to exit.");
+Console.ReadKey();
