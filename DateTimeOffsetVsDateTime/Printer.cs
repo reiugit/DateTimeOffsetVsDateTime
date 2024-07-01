36f5b70afa595f3e9beb1f8b918a4456b4dfa43f
rei <67372053+reiugit@users.noreply.github.com>
2024-07-01 14:13:26 +0200

    Added line breaks to Print methods.

--- DateTimeOffsetVsDateTime/Printer.cs
+++ DateTimeOffsetVsDateTime/Printer.cs
@@ -2,6 +2,9 @@
 
 internal static class Printer
 {
-    internal static void Print(string info, DateTime d) => Console.WriteLine($"  {info + ":",-25}  {d}    (Kind = {d.Kind})");
-    internal static void Print(string info, DateTimeOffset d) => Console.WriteLine($"  {info + ":",-25}  {d}");
+    internal static void Print(string info, DateTime d)
+        => Console.WriteLine($"  {info + ":",-25}  {d}    (Kind = {d.Kind})");
+
+    internal static void Print(string info, DateTimeOffset d)
+        => Console.WriteLine($"  {info + ":",-25}  {d}");
 }
